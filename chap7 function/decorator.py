5288e0053bc489d25540d53a8836f7396b5e05b2
tb1over <nxsfcc@gmail.com>
2018-04-01 21:45:55 +0800

    update

--- chap7 function/decorator.py
+++ chap7 function/decorator.py
@@ -1,16 +1,33 @@
 from datetime import datetime
 
+'''
 def log(func):
     def wrapper(*args, **kw):
         print(' call %s():' % func.__name__)
         return func(*args, **kw)
     return wrapper;
 
+# now = log(now)
 @log
 def now():
     print(datetime.now())
 
-# now = log(now)
 print(now.__name__)
 
+now()
+
+'''
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s(): ' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+@log('excute')
+def now():
+    print(datetime.now())
+print(now.__name__)
 now()
